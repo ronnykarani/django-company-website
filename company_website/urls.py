3032d87122120570734b766fa3e9139964637213
ronnykarani <karanironny25@gmail.com>
2022-06-13 00:16:06 +0300

    blog app

--- company_website/urls.py
+++ company_website/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('', include('home.urls')),
-    #path('blog/', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     
 ]
 
