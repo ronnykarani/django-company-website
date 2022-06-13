ae59408586e8218758ba1655413506a6570ceb46
ronnykarani <karanironny25@gmail.com>
2022-06-13 12:50:28 +0300

    blog posts fetch

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='blog'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.utils import timezone
+from django.contrib import messages
+from django.core.mail import send_mail
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-id')
+    context = {'posts': posts, }
+    return render(request, 'home.html', context)
 
 
 def contact(request):
