3fc1100807aa3b91fba8f6baf5ec5b7ca8f7d363
ronnykarani <karanironny25@gmail.com>
2022-06-15 13:56:03 +0300

    projects

--- home/views.py
+++ home/views.py
@@ -3,12 +3,14 @@ from blog.models import Post
 from django.utils import timezone
 from django.contrib import messages
 from django.core.mail import send_mail
+from project.models import Project
 
 
 # Create your views here.
 def home(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-id')
-    context = {'posts': posts, }
+    projects = Project.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    context = {'posts': posts, 'projects':projects}
     return render(request, 'home.html', context)
 
 
